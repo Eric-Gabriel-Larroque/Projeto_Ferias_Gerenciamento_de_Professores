42edc0e49326cf04ddd6562612f649f53688f6ce
Eric-Gabriel-Larroque <ericgabriellarroque@gmail.com>
2022-01-04 17:52:41 -0300

    removendo dependencias inutilizadas

--- Gerenciamento_de_Professores/src/gerenciamento_de_professores/Main.java
+++ Gerenciamento_de_Professores/src/gerenciamento_de_professores/Main.java
@@ -3,6 +3,6 @@ package gerenciamento_de_professores;
 public class Main extends GUI{
 
     public static void main(String[] args) {
-        new Main().iniciar();
+       new Main().iniciar();
     }
 }
